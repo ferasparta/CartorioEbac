43b0a1eead8df555c9f064b6267c89c41b130709
ferasparta <feracrypto792@gmail.com>
2025-01-28 10:35:48 -0300

    Opção de saida
    Incluso no sistema a opção de saída para o usuário

--- Projeto3.c
+++ Projeto3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>  
 #include <stdlib.h> 
 #include <locale.h> 
+#include <string.h>
 
 int registro()
 {
@@ -59,9 +60,32 @@ int registro()
 
 int consulta()
 {
-	printf("Você escolheu consultar os nomes!\n"); 
+	setlocale(LC_ALL,"Portuguese");
+
+	char cpf[40];
+	char conteudo[200];
+	
+	printf("Digite o CPF a ser consultado: ");
+	scanf("%s",cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL)
+	{
+		printf("Nao foi possivel abrir o arquivo, nao localizado.!\n");
+	}
+	
+	while(fgets(conteudo, 200, file) != NULL)
+{
+		printf("\nEssas sao as informacoes do usuario: ");
+		printf("%s", conteudo);
+		printf("\n\n");
+}
+	
 	system("pause");
 }
+
 int deletar()
 {
 	printf("Você escolheu deletar nomes!\n"); 
@@ -84,6 +108,7 @@ int main()
 		printf("\t1 Registrar nomes\n");
 		printf("\t2 Consultar nomes\n");
 		printf("\t3 Deletar nomes\n\n");
+		printf("\t4 Sair do sistema\n\n");
 		printf("Opção: ");
 
 		scanf("%d", &opcao); 
@@ -104,7 +129,12 @@ int main()
 		case 3:
 		deletar();
 		break;
-
+		
+		case 4:
+		printf("Obrigado por utilizar o sistema!\n");
+		return 0;
+		break;
+		
 		default:
 		printf("Essa opção não está disponivel!\n"); 
 		system("pause");
